a42d3914f7d07d51e8edd25e846ad08ebc5ba24a
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-20 00:05:46 +0000

    feat: Implement Item and Effect abstract class

--- src/Quest/Item.ts
+++ src/Quest/Item.ts
@@ -1,9 +1,13 @@
-import { Effect } from "Quest";
+import { Effect, EffectOption } from "Quest";
 
 export abstract class Item {
   public effect: Effect | null;
 
   constructor(public name: string, public description: string, effect: Effect | null = null) {
     this.effect = effect;
+  }
+
+  public takeEffect(...options: EffectOption[]): boolean {
+    return this.effect?.effect(options) ?? false;
   }
 }
