640480e928e56da919cb04eba8d44b12504356f0
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-20 00:33:11 +0000

    feat: Implement Avatar

--- src/Account/Avatar.ts
+++ src/Account/Avatar.ts
@@ -1,1 +1,10 @@
-export class Avatar {}
+import { IAccountRepository, Player } from "Account";
+
+export enum Race {
+  Egg,
+  Plant,
+}
+
+export class Avatar {
+  constructor(public repo: IAccountRepository, public player: Player, public race: Race) {}
+}
