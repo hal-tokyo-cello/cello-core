8d6ffdc510e3b0f674cf187048240b7d1c99ea1d
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-21 11:53:50 +0000

    build: Expose domain directly

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export * as domain from "./src/domain";
-export * as infrastructure from "./src/infrastructure";
-export * as application from "./src/application";
+export * as domain from "./domain";
+export * as infrastructure from "./infrastructure";
+export * as application from "./application";
