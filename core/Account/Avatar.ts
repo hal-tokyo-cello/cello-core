dd47906effb6ecf3f024eb1b3b9212905de3fdf5
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-23 02:06:07 +0000

    docs: ドメインレイヤーのアカウント関連のクラスをドキュメント

--- core/Account/Avatar.ts
+++ core/Account/Avatar.ts
@@ -1,20 +1,35 @@
 import { Item } from "core";
 
+/**
+ * アバターの種族。
+ */
 export enum Race {
   Egg,
   Plant,
 }
 
+/**
+ * アバターを代表するクラス。
+ */
 export class Avatar {
   /**
-   * Level function is `50x ^ 2`.
+   * アバターのレベル。経験値換算関数は`50x ^ 2`.
    */
   public get level(): number {
     return this.totalExp == 0 ? 0 : Math.sqrt(this.totalExp / 50);
   }
 
+  /**
+   * アバタークラスのコンストラクタ。
+   * @param race アバターの種族
+   * @param totalExp アバターの総計経験値
+   */
   constructor(public race: Race, public totalExp: number) {}
 
+  /**
+   * アイテムをアバターに使用する。
+   * @param items 使うアイテム
+   */
   public applyItem(...items: Item[]): void {
     items.forEach((item) => {
       item.takeEffect();
--- core/Account/User.ts
+++ core/Account/User.ts
@@ -2,11 +2,28 @@ import { Avatar } from "core";
 import crypto from "crypto";
 import { IAccountRepository } from "infrastructure";
 
+/**
+ * ユーザーを代表する抽象クラス。
+ */
 export abstract class User {
+  /**
+   * ユーザーのパスワード。デフォルトはnull、必要の場合はrepoから取得する。
+   */
   public password?: string;
 
+  /**
+   * ユーザークラスのコンストラクタ。
+   * @param repo アカウント関連の情報を載っているレポジトリ
+   * @param accountId アカウントを表すID
+   * @param email アカウントのメールアドレス
+   */
   constructor(public repo: IAccountRepository, public accountId: string, public email: string) {}
 
+  /**
+   * ユーザーをログインする。
+   * @param options ログインのオプション
+   * @returns 成功する場合はtrue; 失敗の場合はfalse
+   */
   public async login(options: LoginOptions): Promise<boolean> {
     if (this.password === undefined) {
       this.password = await this.repo.getUserPassword(this.accountId);
@@ -24,10 +41,22 @@ export abstract class User {
     return result;
   }
 
+  /**
+   * パスワードをハッシュ化する。
+   * @param password 未処理のパスワード
+   * @returns ハッシュ化したパスワード
+   */
   public static hashPassword(password: string): string {
     return crypto.createHash("sha256", {}).update(password).digest("hex");
   }
 
+  /**
+   * 新規ユーザーを登録する。
+   * @param repo アカウント関連の情報を載っているレポジトリ
+   * @param email 新規ユーザーのメールアドレス
+   * @param password 新規ユーザーのパスワード
+   * @returns
+   */
   public static register(repo: IAccountRepository, email: string, password: string): Promise<void> {
     password = this.hashPassword(password);
     return Promise.resolve(
@@ -35,6 +64,12 @@ export abstract class User {
     );
   }
 
+  /**
+   * ユーザーのパスワードを更新する。
+   * 前回と同様のパスワードに指定した場合は失敗する。
+   * @param password 新しいパスワード
+   * @returns 成功する場合はtrue; 失敗する場合はfalse
+   */
   public async updatePassword(password: string): Promise<boolean> {
     this.password = this.password ?? (await this.repo.getUserPassword(this.accountId));
 
@@ -48,6 +83,11 @@ export abstract class User {
     return true;
   }
 
+  /**
+   * ユーザーをプレイヤーに昇進する。
+   * @param avatar 選択されたアバター
+   * @returns 昇進するプレイヤー
+   */
   public upgradeToPlayer(avatar: Avatar): Player {
     const p = new Player(this.repo, this.accountId, this.email);
     p.avatar = avatar;
@@ -55,24 +95,53 @@ export abstract class User {
   }
 }
 
+/**
+ * ログインのオプションを代表するクラス。
+ */
 export class LoginOptions {
+  /**
+   * ログインオプションクラスのコンストラクタ
+   * @param email ユーザーのメールアドレス
+   * @param password ユーザーのパスワード
+   */
   constructor(public email: string, public password: string) {}
 }
 
+/**
+ * プレイヤーを代表するクラス。
+ */
 export class Player extends User {
+  /**
+   * プレイヤーが育成しているアバター
+   */
   public avatar!: Avatar;
 
+  /**
+   * プレイヤーのレベル、アバターと同じ
+   */
   public get level(): number {
     return this.avatar.level;
   }
 
   /**
+   * プレイヤークラスのコンストラクタ。
    * プレイヤーをインスタンス化する場合は`New`を使ってください。
+   * @param repo アカウント関連の情報を載っているレポジトリ
+   * @param accountId アカウントのID
+   * @param email メールアドレス
    */
   constructor(repo: IAccountRepository, accountId: string, email: string) {
     super(repo, accountId, email);
   }
 
+  /**
+   * プレイヤーをインスタンス化する静的メソッド。
+   * プレイヤーをインスタンス化するにはコンストラクタではなく、`New`を使用すべき。
+   * @param repo アカウント関連の情報を載っているレポジトリ
+   * @param accountId アカウントのID
+   * @param email メールアドレス
+   * @returns インスタンス化したプレイヤー
+   */
   public static async New(repo: IAccountRepository, accountId: string, email: string): Promise<Player> {
     const p = new Player(repo, accountId, email);
     p.avatar = await p.repo.getAvatar(p.accountId);
