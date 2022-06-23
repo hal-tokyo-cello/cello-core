9ef45d88768d4b5b2abced26ce788f26082c232d
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-23 02:43:05 +0000

    docs: インフラストラクチャーレイヤーのインターフェースをドキュメント

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,29 +2,71 @@ import { Answer, Avatar, Item, Player, Quest, User } from "core";
 
 export type Identifier = string | number;
 
+/**
+ * クエスト関連の情報を載っているレポジトリ。
+ */
 export interface IQuestRepository {
-  getQuest: (id: Identifier) => Promise<Quest>;
-  getAnswer: (id: Identifier) => Promise<Answer>;
-  getItem: (id: Identifier) => Promise<Item>;
+  /**
+   * クエストのを取得する。
+   * @param id クエストのID
+   */
+  getQuest(id: Identifier): Promise<Quest>;
+  /**
+   * 解答を取得する。
+   * @param id 解答のID
+   */
+  getAnswer(id: Identifier): Promise<Answer>;
+  /**
+   * アイテムを取得する。
+   * @param id アイテムのID
+   */
+  getItem(id: Identifier): Promise<Item>;
 }
 
+/**
+ * アカウント関連の情報を載っているレポジトリ。
+ */
 export interface IAccountRepository {
-  getUser(id: Identifier): Promise<User>;
-  getUserPassword(id: Identifier): Promise<string>;
-  getPlayer(id: Identifier): Promise<Player>;
   /**
-   * プレイヤーのIDからアバターを取得する。
-   * @param player プレイヤーのID
+   * ユーザーを取得する。
+   * @param id ユーザーのID
    */
-  getAvatar(player: Identifier): Promise<Avatar>;
+  getUser(id: Identifier): Promise<User>;
+  /**
+   * ユーザーを格納する。
+   * @param user ユーザーオブジェクト
+   */
+  registerNewUser(user: User): Promise<void>;
+  /**
+   * ユーザーのパスワードを取得する。
+   * @param id ユーザーのID
+   */
+  getUserPassword(id: Identifier): Promise<string>;
   /**
    * ユーザーのパスワードを更新する。
    * @param user ユーザーのID
    * @param password 新しいパスワードのハッシュ
    */
   updateUserPassword(user: Identifier, password: string): Promise<void>;
+  /**
+   * プレイヤーを取得する。
+   * @param id プレイヤーのID
+   */
+  getPlayer(id: Identifier): Promise<Player>;
+  /**
+   * プレイヤーのIDからアバターを取得する。
+   * @param player プレイヤーのID
+   */
+  getAvatar(player: Identifier): Promise<Avatar>;
 
+  /**
+   * 最後にログインを試みた時日を記録する。
+   * @param timestamp Unixタイムスタンプ
+   */
   setLastLoginAttempt(timestamp: number): Promise<void>;
+  /**
+   * 最後のログイン時日を記録する。
+   * @param timestamp Unixタイムスタンプ
+   */
   setLastLogin(timestamp: number): Promise<void>;
-  registerNewUser(user: User): Promise<void>;
 }
