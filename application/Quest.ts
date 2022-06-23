ede6efd896bd7caac1577ae588ef4c016674d15b
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-23 02:00:14 +0000

    docs: アプリケーションレイヤーのクラスをドキュメント

--- application/Quest.ts
+++ application/Quest.ts
@@ -1,17 +1,44 @@
+/**
+ * クエストを取得するリクエストのbody部。
+ */
 export interface QuestRequest {}
 
 interface QuestSimple {
+  /**
+   * クエストのタイトル
+   */
   title: string;
+  /**
+   * クエストを突破して得られる経験値
+   */
   exp: number;
+  /**
+   * クエストのジャンル
+   */
   genre: string;
 }
 
+/**
+ * クエストを取得したレスポンスのbody部。
+ */
 export interface QuestResponse {
+  /**
+   * クエストの要約
+   */
   quest: QuestSimple;
 }
 
+/**
+ * クエストのリストを取得するリクエストのbody部。
+ */
 export interface QuestListRequest {}
 
+/**
+ * クエストのリストを取得したレスポンスのbody部。
+ */
 export interface QuestListResponse {
+  /**
+   * クエストの要約。
+   */
   quests: QuestSimple;
 }
