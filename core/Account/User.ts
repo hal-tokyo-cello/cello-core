1dfd811ddea437896763e9a81f9fd4e9b81eff91
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-23 03:12:30 +0000

    refactor: ユーザーの昇格方法を見直し

--- core/Account/User.ts
+++ core/Account/User.ts
@@ -1,4 +1,4 @@
-import { Avatar } from "core";
+import { Avatar, Race } from "core";
 import crypto from "crypto";
 import { IAccountRepository } from "infrastructure";
 
@@ -48,15 +48,27 @@ export abstract class User {
     return true;
   }
 
-  public upgradeToPlayer(avatar: Avatar): Player {
+  public upgradeToPlayer(option: PlayerUpgradeOption): Promise<void> {
     const p = new Player(this.repo, this.accountId, this.email);
-    p.avatar = avatar;
-    return p;
+    p.avatar = new Avatar(option.race, option.exp);
+    return this.repo.upgradeUserToPlayer(this, p);
   }
 }
 
 export class LoginOptions {
   constructor(public email: string, public password: string) {}
+}
+
+/**
+ * プレイヤーを昇格するオプションを代表するクラス。
+ */
+export class PlayerUpgradeOption {
+  /**
+   * プレイヤー昇格オプションのコンストラクタ。
+   * @param race アバターの種族
+   * @param exp 初期の総計経験値
+   */
+  constructor(public race: Race, public exp: number = 0) {}
 }
 
 export class Player extends User {
