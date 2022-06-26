81aef13806fb2c9b513afc4a151ea2d0e8c7a66d
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-26 17:24:57 +0000

    feat: 認証APIをexport

--- application/Auth.ts
+++ application/Auth.ts
@@ -1,18 +1,30 @@
+/**
+ * サインインのリクエストのbody部。
+ */
 export interface SignInRequest {
   email: string;
   password: string;
 }
 
+/**
+ * サインインのレスポンスのbody部。
+ */
 export interface SignInResponse {
   succeed: boolean;
   error?: string;
 }
 
+/**
+ * サインアップのリクエストのbody部。
+ */
 export interface SignUpRequest {
   email: string;
   password: string;
 }
 
+/**
+ * サインアップのレスポンスのbody部。
+ */
 export interface SignUpResponse {
   succeed: boolean;
   error?: string;
